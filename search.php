38ee33d6aec48a0718083dbfc57ce2cf1e19adf9
Fenrir <fenrireclair@gmail.com>
2021-11-22 18:54:22 +0900

    search button(ok)

---
+++ search.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcone to Doodle </title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"
+</head>
+<body>
+    <div class="wrapper">
+
+        <div class="header">
+
+            <div class="headerContent">
+                
+                <div class="logoContainer">
+                    <a href="index.html">
+                        <img src="assets/images/doodleLogo.png">
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+                    <form action="search.php" method="get">
+                        <div class="searchBarContainer">
+                            <input class="searchBox" type="text" name="term">
+                            <button class="searchButton"> 
+                                <img src="assets\images\icons\search.png"
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</body>
+</html>
