7ae82c52b1d95c8d8c3df2417b8d2932d466240c
Fenrir <fenrireclair@gmail.com>
2021-12-06 16:21:57 +0900

    新增註解

--- crawled.php
+++ crawled.php
@@ -1,7 +1,7 @@
 <?php 
 include ("classes/DomDocumentParser.php");
 function folloeLinks($url) {
-    $parser = new DomDocumentParser($url);
+    $parser = new DomDocumentParser($url); // call class
 }
 $url = "https://www.udemy.com/";
 folloeLinks($url);
