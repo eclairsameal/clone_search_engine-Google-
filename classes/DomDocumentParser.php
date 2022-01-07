90a0baff55956b4ff180bc81735aec84e31fe79a
Fenrir <fenrireclair@gmail.com>
2022-01-07 11:11:21 +0900

    新增註解

--- classes/DomDocumentParser.php
+++ classes/DomDocumentParser.php
@@ -1,8 +1,17 @@
 <?php 
 class DomDocumentParser {
+    private $doc;
     public function __construct($url) {
-        echo "URL: $url";
+        // 建構子()
+        // echo "URL: $url";
+        $options = array (
+            'http'=>array('method'=>"GET", 'header'=>"User-Agent: doodleBot/0.1\n")
+        ); // 'http':key ，他有個array型態的資料
+        $context = stream_context_create($options);
+
+        $doc = new DomDocument();
+        @ $doc->loadHTML(file_get_contents($url, false, $context));
     }
 }
-
 ?>
+
